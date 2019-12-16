e87ee0a79d170da5f25bf8349ded3877c0bed327
Jorge Machado <jorge.vmachado@hotmail.com>
2019-12-16 19:34:51 -0300

    adding EntryFormComponent

--- src/app/pages/entries/category-form/entry-form.component.ts
+++ src/app/pages/entries/category-form/entry-form.component.ts
@@ -65,6 +65,12 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
       id: [null],
       name: [null, [Validators.required, Validators.minLength(2)]],
       description: [null],
+      type: [null, [Validators.required]],
+      amount: [null, [Validators.required]],
+      date: [null, [Validators.required]],
+      paid: [null, [Validators.required]],
+      categoryId: [null, [Validators.required]],
+
 
     });
   }
@@ -83,10 +89,10 @@ export class EntryFormComponent implements OnInit, AfterContentChecked {
 
   private setPageTitle() {
     if(this.currentAction === 'new') {
-      this.pageTitle = 'Cadastro de Nova Categoria'
+      this.pageTitle = 'Cadastro de Novo Lançamento'
     } else {
       const entryName = this.entry.name || '';
-      this.pageTitle = 'Editando Categoria: ' + entryName
+      this.pageTitle = 'Editando Lançamento: ' + entryName
     }
   }
 

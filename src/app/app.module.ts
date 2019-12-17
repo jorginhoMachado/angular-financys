8b071d8c696727e27bf93c84f387eae1efdad916
Jorge Machado <jorge.vmachado@hotmail.com>
2019-12-17 17:32:57 -0300

    project restructuring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
-import {InMemoryDatabase} from "./in-memory-database";
-import {HttpClientModule} from "@angular/common/http";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {CoreModule} from "./core/core.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
+    CoreModule,
     AppRoutingModule,
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
-
   ],
   providers: [],
   bootstrap: [AppComponent]

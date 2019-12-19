aef8eb75887f3a3507a7c0d7ea6fe890c2d3891e
Jorge Machado <jorge.vmachado@hotmail.com>
2019-12-19 19:30:27 -0300

    adding Reports template

--- src/app/pages/reports/reports/reports.component.ts
+++ src/app/pages/reports/reports/reports.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Category} from "../../categories/shared/category.model";
+import {Entry} from "../../entries/shared/entry.model";
+import {EntryService} from "../../entries/shared/entry.service";
+import {CategoryService} from "../../categories/shared/category.service";
 
 @Component({
   selector: 'app-reports',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
-  constructor() { }
+  expenseTotal: any = 0;
+  revenueTotal: any = 0;
+  balance: any = 0;
+
+  expenseChartData: any;
+  revenueChartData: any;
+
+  chartOptions = {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  };
+
+  categories: Category[] = [];
+  entries: Entry[] = [];
+
+  @ViewChild('month') month: ElementRef = null;
+  @ViewChild('year') year: ElementRef = null;
+
+  constructor(private entryService: EntryService, private categoryService: CategoryService) { }
 
   ngOnInit() {
+    this.categoryService.getAll()
+      .subscribe(categories => this.categories = categories);
+  }
+
+  generateReports() {
+
   }
 
 }
